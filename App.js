77b334b66a22877c79da2e6a645d900874549d3c
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-10-13 17:03:36 +0200

    Navigation

--- App.js
+++ App.js
@@ -7,24 +7,29 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, StatusBar, View} from 'react-native';
+import {Platform, StyleSheet, StatusBar, View, Text} from 'react-native';
 import {MealList} from './src/app/components/meals';
+import { createStackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: MealList
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+     <View style={styles.container}>
         <StatusBar backgroundColor="#FCA10F" barStyle="light-content" />
-        <MealList />
-      </View>
+        <RootStack />
+     </View>
     );
   }
 }
@@ -33,7 +38,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F7F7F7',
   },
 });
--- src/app/components/meals/MealList.js
+++ src/app/components/meals/MealList.js
@@ -1,26 +1,34 @@
 import React, {Component} from 'react';
-import {FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
 import {Meal} from './Meal.js';
 
 export class MealList extends Component {
+    static navigationOptions = {
+        title: 'McGo',
+    };
     render() {
         return (
-            <FlatList 
-                contentContainerStyle={styles.list}
-                numColumns={2}
-                data = {[
-                    {key: "Jambon-Beurre", thumbnailUri: 'http://www.disneyfoodblog.com/wp-content/uploads/2013/07/ham-and-cheese-baguette-Les-Halles.jpg'},
-                    {key: "Jambon-Beurre", thumbnailUri: 'http://www.disneyfoodblog.com/wp-content/uploads/2013/07/ham-and-cheese-baguette-Les-Halles.jpg'},
-                    {key: "Jambon-Beurre", thumbnailUri: 'http://micheltanguy.com/wp-content/uploads/2017/03/jambon-beurre2-1.jpg'}
-                ]}
+            <View style={styles.container}>
+                <FlatList 
+                    contentContainerStyle={styles.list}
+                    numColumns={2}
+                    data = {[
+                        {key: "Jambon-Beurre", thumbnailUri: 'http://www.disneyfoodblog.com/wp-content/uploads/2013/07/ham-and-cheese-baguette-Les-Halles.jpg'},
+                        {key: "Jambon-Beurre", thumbnailUri: 'http://www.disneyfoodblog.com/wp-content/uploads/2013/07/ham-and-cheese-baguette-Les-Halles.jpg'},
+                        {key: "Jambon-Beurre", thumbnailUri: 'http://micheltanguy.com/wp-content/uploads/2017/03/jambon-beurre2-1.jpg'}
+                    ]}
 
-                renderItem = {({item}) => <Meal style={styles.item} name={item.key} thumbnailUri={item.thumbnailUri} />}
-            />
+                    renderItem = {({item}) => <Meal style={styles.item} name={item.key} thumbnailUri={item.thumbnailUri} />}
+                />
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
     list: {
         flexDirection: 'column',
     },
