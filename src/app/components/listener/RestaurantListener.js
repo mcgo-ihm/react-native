2091436f7618abb5e0066a326fb68bb8145378e8
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-11-06 22:10:17 +0100

    Bipper

--- src/app/components/listener/RestaurantListener.js
+++ src/app/components/listener/RestaurantListener.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import {View, Alert, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import {PushNotificationIOS} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
 export class RestaurantListener extends Component {
     constructor(props) {
         super(props);
+        this.bipper = false;
+        this.longitude = 7;
+        this.latitude = 43.55;
+        this.threshold = 0.005;
     }
 
     componentWillMount() {
@@ -21,6 +25,8 @@ export class RestaurantListener extends Component {
                 notification.finish(PushNotificationIOS.FetchResult.NoData);
             },
 
+            requestPermissions: true,
+
             // IOS ONLY (optional): default: all - Permissions to register.
             permissions: {
                 alert: true,
@@ -28,15 +34,28 @@ export class RestaurantListener extends Component {
                 sound: true
             },
 
+            visibility: "public",
+            priority: "high",
+            importance: "high"
+
         });
 
         this.watchId = navigator.geolocation.watchPosition(
             (position) => {
-                if (Math.abs(position.coords.longitude - 7) < 0.005 && Math.abs(position.coords.latitude - 43.55) < 0.005) {
-                    //Alert.alert(position.coords.longitude + " " + position.coords.latitude);
+                if (!this.bipper && Math.abs(position.coords.longitude - this.longitude) < this.threshold
+                  && Math.abs(position.coords.latitude - this.latitude) < this.threshold) { // activate bipper
+                    this.bipper = true;
                     PushNotification.localNotification({
-                        title: "Hey!",
-                        message: position.coords.longitude + " " + position.coords.latitude
+                        title: "Bipper activé",
+                        message: "Votre téléphone sonnera quand votre commande sera prête.",
+                    });
+                } else if (this.bipper
+                  && !(Math.abs(position.coords.longitude - this.longitude) < this.threshold
+                  && Math.abs(position.coords.latitude - this.latitude) < this.threshold)) { //desactivate bipper
+                    this.bipper = false;
+                    PushNotification.localNotification({
+                        title: "Bipper désactivé",
+                        message: "Votre téléphone repasse en mode normal.",
                     });
                 }
             });
